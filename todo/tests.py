ed739b6dbcc0a6f7544338b834b2568143571f6b
1F10240304 <s1f102403047@iniad.org>
2025-07-03 16:49:34 +0900

    requirements.txtを追加

--- todo/tests.py
+++ todo/tests.py
@@ -16,12 +16,11 @@ class TaskModelTestCase(TestCase):
         task = Task(title='task1', due_at=due)
         task.save()
 
-
         task = Task.objects.get(pk=task.pk)
         self.assertEqual(task.title, 'task1')
         self.assertFalse(task.completed)
         self.assertEqual(task.due_at, due)
-    
+
 
     def test_create_task2(self):
         task = Task(title='task2')
@@ -38,7 +37,7 @@ class TaskModelTestCase(TestCase):
         current = timezone.make_aware(datetime(2024, 6, 30, 0, 0, 0))
         task = Task(title='task1', due_at=due)
         task.save()
-        
+    
         self.assertFalse(task.is_overdue(current))
 
 
@@ -105,3 +104,4 @@ class TodoViewTestCase(TestCase):
         self.assertEqual(response.templates[0].name, 'todo/index.html')
         self.assertEqual(response.context['tasks'][0], task1)
         self.assertEqual(response.context['tasks'] [1], task2)
+
